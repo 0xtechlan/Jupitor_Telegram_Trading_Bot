1eb7a97003f47a14bc08ebfb119e3f0bb6959213
0xtechlan <hitechlan1000@gmail.com>
2025-07-16 13:48:34 +0900

    feat: help

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,11 @@ bot.onText(/\/start/, async (msg) => {
   bot.sendMessage(userId, "Welcome! Set token address:");
 });
 
+// help command
+bot.onText(/\/help/, async (msg) => {
+   const userId = msg.from!.id;
+  bot.sendMessage(userId, "Welcom to help center:");
+});
 // Handle text messages (wallet setup, buy, and sell)
 bot.on("message", async (msg) => {
   const userId = msg.from!.id;
